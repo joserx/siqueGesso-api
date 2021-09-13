231579a9295ffd1d62a7206e6bd787854805381a
felipemelonunes09 <felipe.melonunes09@gmail.com>
2021-09-13 10:26:40 -0300

    stock entity

---
+++ src/stock/dto/update-stock.dto.ts
@@ -0,0 +1,8 @@
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateStockDto } from './create-stock.dto';
+
+export class UpdateStockDto extends PartialType(CreateStockDto) {
+    min: number;
+    max: number;
+    current: number;
+}
