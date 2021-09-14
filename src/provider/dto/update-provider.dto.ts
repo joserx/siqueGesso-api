0a9728b6f8a588e6abe61e3834926998328ca90a
felipemelonunes09 <felipe.melonunes09@gmail.com>
2021-09-14 16:47:41 -0300

    provider dtos

--- src/provider/dto/update-provider.dto.ts
+++ src/provider/dto/update-provider.dto.ts
@@ -1,4 +1,25 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { AddressEntity } from 'src/entities/address.entity';
+import { PaymentCondition } from 'src/provider/entities/payment_condition.entity';
 import { CreateProviderDto } from './create-provider.dto';
+import { Contact } from 'src/provider/entities/contact.entity';
 
-export class UpdateProviderDto extends PartialType(CreateProviderDto) {}
+
+export class UpdateProviderDto extends PartialType(CreateProviderDto) {
+
+    category: string;
+    cnpj: string;
+    fantasy_name: string;
+    social_reason: string;
+    tribute_code: string;
+    contribuinte: string;
+    state_registration: string;
+    is_exempt: boolean;
+    municipal_registration: string;
+    address: AddressEntity;
+    contacts: Array<Contact>
+    payment_condition: Array<number>
+    first_payment: Date;
+    last_payment: Date;
+    notes: string;
+}
