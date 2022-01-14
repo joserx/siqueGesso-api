c1be415d2e3f72492cc4104235c6b29178581ce3
joserx <jose.hamilton2050@gmail.com>
2022-01-14 01:27:15 -0300

    rota para colab ativos

--- src/vt/vt.service.ts
+++ src/vt/vt.service.ts
@@ -7,32 +7,35 @@ import { UpdateVtDto } from './dto/update-vt.dto';
 
 @Injectable()
 export class VtService {
-
   constructor(
     @InjectRepository(VtEntity)
-    private readonly vtRepository: Repository<VtEntity>
-  ){}
+    private readonly vtRepository: Repository<VtEntity>,
+  ) {}
 
   async create(data) {
-    let vt = this.vtRepository.create(data)
-    console.log(data)
-    return await this.vtRepository.save(vt)
+    let vt = this.vtRepository.create(data);
+    console.log(data);
+    return await this.vtRepository.save(vt);
   }
 
   async findAll() {
-    return await this.vtRepository.find()
+    return await this.vtRepository.find();
+  }
+
+  async findAtivo() {
+    return await this.vtRepository.find({ where: { disabled: false } });
   }
 
   async findOne(id: number) {
-    if(id && Number(id)){
+    if (id && Number(id)) {
       return await this.vtRepository.findOne(id);
-    }else{
+    } else {
       throw new HttpException('No id provided', 500);
     }
   }
 
   async update(id: number, data: any) {
-    await this.vtRepository.update(id, data)
+    await this.vtRepository.update(id, data);
   }
 
   async remove(id: number) {
